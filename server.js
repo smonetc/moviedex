483c2c9f945cedcdc63343f98bdc0f06d4eb21ce
Monet Cooper <smomoc@monets-mbp-2.lan>
2020-10-01 19:43:49 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const cors = require('cors')
 const helmet = require('helmet')
 const MOVIEDEX = require('./movies-data-small.json')
 
-console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
 
 app.use(function handleBearerToken(req,res,next){
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log(authToken)
+   
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
       return res.status(401).json({ error: 'Unauthorized request' })
     }
@@ -45,8 +45,18 @@ app.get('/movie',function handleGetMovie(req,res){
     res.json(response)
 })
 
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
 
-const PORT = 8000
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
